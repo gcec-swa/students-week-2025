f011b57750cd9450c1642b7a6374da12b0efd391
gcec-swa <jagriti@gcect.ac.in>
2025-01-04 14:46:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showSection(sectionId) {
+    const sections = document.querySelectorAll('.content');
+    sections.forEach(section => {
+        section.style.display = 'none';
+    });
+    document.getElementById(sectionId).style.display = 'block';
+}
+
+window.onload = function() {
+    showSection('home');
+};
