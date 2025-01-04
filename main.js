1cf66fdc566889a07b01a867404c585539ce80c2
biswas08433 <BISWAS08433@GMAIL.COM>
2025-01-04 17:19:21 +0530

    try: relative path

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ window.onload = function () {
 document.addEventListener("DOMContentLoaded", () => {
   const markdownContainer = document.getElementById("markdown-content");
 
-  fetch("markdowns/index.md")
+  fetch("/student-week-2025/markdowns/index.md")
     .then((response) => {
       if (!response.ok) {
         throw new Error("Failed to load Markdown file");
